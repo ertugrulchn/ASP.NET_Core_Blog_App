b4c944982f9f5181c1eaa0e375b37fa8dd9fc6dd
Ertuğrul Emre Cihan <cihanertugrulemre368@gmail.com>
2021-10-03 19:22:12 +0300

    03.10.2021

---
+++ ProgrammersBlog.Shared/Utilities/Results/Concrete/Result.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProgrammersBlog.Shared.Utilities.Results.Abstract;
+using ProgrammersBlog.Shared.Utilities.Results.ComplexTypes;
+
+namespace ProgrammersBlog.Shared.Utilities.Results.Concrete
+{
+    public class Result : IResult
+    {
+        public Result(ResultStatus resultStatus)
+        {
+            ResultStatus = resultStatus;
+        }
+
+        public Result(ResultStatus resultStatus, string mesage)
+        {
+            ResultStatus = resultStatus;
+            Message = Message;
+        }
+
+        public Result(ResultStatus resultStatus, string mesage, Exception exception)
+        {
+            ResultStatus = resultStatus;
+            Message = Message;
+            Exception = exception;
+        }
+
+        public ResultStatus ResultStatus { get; }
+
+        public string Message { get; }
+
+        public Exception Exception { get; }
+    }
+}
