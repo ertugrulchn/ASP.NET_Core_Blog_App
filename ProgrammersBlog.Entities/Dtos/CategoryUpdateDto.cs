320242d700e3f7c6570cf9d43770687150187ed7
Ertuğrul Emre Cihan <cihanertugrulemre368@gmail.com>
2021-10-04 07:53:36 +0300

    04.10.2021

--- ProgrammersBlog.Entities/Dtos/CategoryUpdateDto.cs
+++ ProgrammersBlog.Entities/Dtos/CategoryUpdateDto.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,32 @@ namespace ProgrammersBlog.Entities.Dtos
 {
     public class CategoryUpdateDto
     {
+        [Required]
+        public int Id { get; set; }
+
+        [DisplayName("Kategori Adı")]
+        [Required(ErrorMessage = "{0} Boş Bırakılmamalıdır")]
+        [MaxLength(70, ErrorMessage = "{0} {1} Karakterden Büyük Olmamalıdır.")]
+        [MinLength(2, ErrorMessage = "{0} {2} Karakterden Az Olmamalıdır.")]
+
+        public string Name { get; set; }
+
+        [DisplayName("Kategori Açıklaması")]
+        [MaxLength(500, ErrorMessage = "{0} {1} Karakterden Büyük Olmamalıdır.")]
+        [MinLength(2, ErrorMessage = "{0} {2} Karakterden Az Olmamalıdır.")]
+        public string Description { get; set; }
+
+        [DisplayName("Kategori Özel Not Alanı")]
+        [MaxLength(500, ErrorMessage = "{0} {1} Karakterden Büyük Olmamalıdır.")]
+        [MinLength(2, ErrorMessage = "{0} {2} Karakterden Az Olmamalıdır.")]
+        public string Note { get; set; }
+
+        [DisplayName("Aktif Mi ?")]
+        [Required(ErrorMessage = "{0} Boş Bırakılmamalıdır")]
+        public bool IsActive { get; set; }
+
+        [DisplayName("Silindi Mi ?")]
+        [Required(ErrorMessage = "{0} Boş Bırakılmamalıdır")]
+        public bool IsDeleted { get; set; }
     }
 }
