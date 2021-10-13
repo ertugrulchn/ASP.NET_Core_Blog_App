d531a1f8c0f67451f68ed4e6f942030b4dcbaf7a
Ertuğrul Emre Cihan <cihanertugrulemre368@gmail.com>
2021-10-13 16:42:45 +0300

    13.10.2021

--- ProgrammersBlog.Mvc/Areas/Admin/Controllers/CategoryController.cs
+++ ProgrammersBlog.Mvc/Areas/Admin/Controllers/CategoryController.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProgrammersBlog.Entities.Dtos;
+using ProgrammersBlog.Mvc.Areas.Admin.Models;
 using ProgrammersBlog.Services.Abstract;
+using ProgrammersBlog.Shared.Utilities.Extensions;
 using ProgrammersBlog.Shared.Utilities.Results.ComplexTypes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace ProgrammersBlog.Mvc.Areas.Admin.Controllers
@@ -22,6 +26,35 @@ namespace ProgrammersBlog.Mvc.Areas.Admin.Controllers
         {
             var result = await _categoryService.GetAll();
             return View(result.Data);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return PartialView("_CategoryAddPartial");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(CategoryAddDto categoryAddDto)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _categoryService.Add(categoryAddDto, "Ertuğrul Emre Cihan");
+                if (result.ResultStatus == ResultStatus.Success)
+                {
+                    var categoryAddAjaxModel = JsonSerializer.Serialize(new CategoryAddAjaxViewModel
+                    {
+                        CategoryDto = result.Data,
+                        CategoryAddPartial = await this.RenderViewToStringAsync("_CategoryAddPartial", categoryAddDto),
+                    });
+                    return Json(categoryAddAjaxModel);
+                }
+            }
+            var categoryAddAjaxErrorModel = JsonSerializer.Serialize(new CategoryAddAjaxViewModel
+            {
+                CategoryAddPartial = await this.RenderViewToStringAsync("_CategoryAddPartial", categoryAddDto),
+            });
+            return Json(categoryAddAjaxErrorModel);
         }
     }
 }
